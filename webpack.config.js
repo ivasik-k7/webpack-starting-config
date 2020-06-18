614ed0318915a88b702044ea8b534d7f93f498aa
ivasik-k7 <jeanlacroix@ukr.net>
2020-06-18 10:57:00 +0300

    .a

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    mode: "development",
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                },
+            },
+        },
+        {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                {
+                    loader: "css-loader",
+                },
+                {
+                    loader: "postcss-loader",
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("sass"),
+                    },
+                },
+            ],
+        },
+    ],
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/bundle.css",
+        }),
+    ],
+};
